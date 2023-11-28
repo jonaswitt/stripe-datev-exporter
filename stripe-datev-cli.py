f7963ba23902cc57d23273ed6b69aae9a9275cb1
Jonas Witt <mail@jonaswitt.com>
2023-11-28 11:03:11 +0100

    warn when credit note or voided invoice requires re-download of previous month
    see #9

--- stripe-datev-cli.py
+++ stripe-datev-cli.py
@@ -228,6 +228,29 @@ class StripeDatevCli(object):
       with open(filePath, "wb") as fp:
         fp.write(r.content)
 
+    # Warnings about changes to earlier invoices
+
+    for invoice in stripe.Invoice.list(
+        created={
+            "lt": int(fromTime.timestamp()),
+            "gte": int((fromTime - 6 * datedelta.MONTH).timestamp()),
+        },
+    ).auto_paging_iter():
+      if (invoice.status_transitions.voided_at and datetime.fromtimestamp(
+        invoice.status_transitions.voided_at, timezone.utc) >= fromTime) or (invoice.status_transitions.marked_uncollectible_at and datetime.fromtimestamp(
+            invoice.status_transitions.marked_uncollectible_at, timezone.utc) >= fromTime
+        ):
+        print("Warning: found earlier invoice {} voided or marked uncollectible in this month, consider downloading {} again".format(invoice.id, datetime.fromtimestamp(
+            invoice.status_transitions.finalized_at, timezone.utc).astimezone(stripe_datev.config.accounting_tz).strftime("%Y-%m")))
+
+    creditNotes = stripe_datev.invoices.listCreditNotes(fromTime, toTime)
+    for creditNote in creditNotes:
+      invoiceFinalized = datetime.fromtimestamp(
+        creditNote.invoice.status_transitions.finalized_at, timezone.utc).astimezone(stripe_datev.config.accounting_tz)
+      if invoiceFinalized < fromTime:
+        print("Warning: found credit note {} for earlier invoice, consider downloading {} again".format(
+          creditNote.number, invoiceFinalized.strftime("%Y-%m")))
+
   def validate_customers(self, argv):
     stripe_datev.customer.validate_customers()
 
